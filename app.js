ca3419d6b0e85cfebdee62d9167d0c95220ef5d8
Daniel Kenan Slinda <sdanielkenan@gmail.com>
2021-05-20 21:35:23 +0200

    app file edited

--- app.js
+++ app.js
@@ -21,7 +21,37 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+// 
 
+// const express = require('express');
+const {
+  exec
+} = require('child_process');
+const app = express();
+const port = process.env.PORT || 3000
+app.use(express.static('public'));
+app.use(express.urlencoded({
+  extended: false
+}))
+app.get('/' , (req , res)=>{
+ res.sendFile(__dirname+ '/index.html') 
+})
+app.get('/mail',(req , res)=>{
+  res.sendFile(__dirname+'/email.html')
+})
+app.post('/new-message', (req, res) => {
+  let email = req.body.email;
+  let msg = req.body.message;
+
+  exec(`python ${__dirname}/email-bridge.py ${email} ${msg}`, (err, stdout) => {
+      if (err) {
+          console.log(err);
+          return
+      }
+      console.log(stdout)
+  })
+})
+// 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
