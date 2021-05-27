d86b2dd4b3b4f41edd7b859209bed74544c9ea3b
Daniel Kenan Slinda <sdanielkenan@gmail.com>
2021-05-27 15:16:00 +0200

    commit

--- email-bridge.py
+++ email-bridge.py
@@ -1,18 +1,26 @@
+import os
+
+try:
+  from mailer import Mailer
+except :
+  os.system('pip install quick-mailer')
+
+
 from mailer import Mailer
 from sys import argv
 from passwords import *
 
 user = {
-  'email' : argv[1][0] ,
-  'message' : argv[2][0] ,
-  'name' : argv[3][0]
+  'email' : argv[1] ,
+  'message' : argv[2] ,
+  'name' : argv[3] ,
 }
 
-msg = f"{user.name}\n{user.message}"
+msg = f"{user['name']}\n{user['message']}"
 
 try :
   mail = Mailer(email=DEV_EMAIL, password = PASSWORD)
-  mail.send(receiver=RECEIVER_EMAIL, subject = user.email, message=msg)
+  mail.send(receiver=RECEIVER_EMAIL, subject = user['email'], message=msg)
   print("Email Sent")
 except:
     print('EMAIL NOT SENT')
