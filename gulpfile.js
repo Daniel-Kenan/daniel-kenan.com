60b8a236c98cac45e91a361fdcae633fad37eb00
Daniel Kenan Slinda <sdanielkenan@gmail.com>
2021-06-27 10:35:03 +0200

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,13 @@ const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano') ;
 const gulp = require('gulp');
-const {src, dest} = require('gulp');
+const {src, dest, watch} = require('gulp');
 
 function modify_css(){
     return src('./stylesheets/*.css').pipe(postcss([autoprefixer(),cssnano()])).pipe(dest('./public/stylesheets'));
 }
-
-exports.default = modify_css
+function watchFile(){
+    watch('./stylesheets/*.css',modify_css)
+}
+exports.default = watchFile
 
