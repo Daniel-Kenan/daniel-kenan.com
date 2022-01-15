9d611726ffe318285f5ee9ea93c99e134580010f
Daniel Kenan Slinda <sdanielkenan@gmail.com>
2022-01-15 11:02:17 +0200

    hh

--- public/javascripts/scroll.js
+++ public/javascripts/scroll.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 let prevScrollpos = pageYOffset,
 header = document.getElementsByClassName('menu')[0];
@@ -13,3 +14,20 @@ window.onscroll = () => {
     }
     prevScrollpos = currentScrollPos;
 }
+=======
+
+let prevScrollpos = pageYOffset,
+header = document.getElementsByClassName('menu')[0];
+window.onscroll = () => {
+    var currentScrollPos = pageYOffset;
+
+    if (prevScrollpos > currentScrollPos) {
+        header.style.top = "0";
+    } else {
+        header.style.top = "-100px";
+        
+        // menu_opt.setAttribute("src","/images/PinClipart.com_menu-clip-art_1128307.png");
+    }
+    prevScrollpos = currentScrollPos;
+}
+>>>>>>> d835ef45b91d83b76f89cbc20bbd68887a7a60a4
