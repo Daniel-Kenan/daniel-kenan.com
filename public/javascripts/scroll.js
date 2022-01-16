cc3beb94c4a42886dba25d4c0cbd3b196302d497
Daniel Kenan Slinda <sdanielkenan@gmail.com>
2022-01-16 09:19:11 +0200

    changes

--- public/javascripts/scroll.js
+++ public/javascripts/scroll.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 let prevScrollpos = pageYOffset,
 header = document.getElementsByClassName('menu')[0];
@@ -14,20 +13,3 @@ window.onscroll = () => {
     }
     prevScrollpos = currentScrollPos;
 }
-=======
-
-let prevScrollpos = pageYOffset,
-header = document.getElementsByClassName('menu')[0];
-window.onscroll = () => {
-    var currentScrollPos = pageYOffset;
-
-    if (prevScrollpos > currentScrollPos) {
-        header.style.top = "0";
-    } else {
-        header.style.top = "-100px";
-        
-        // menu_opt.setAttribute("src","/images/PinClipart.com_menu-clip-art_1128307.png");
-    }
-    prevScrollpos = currentScrollPos;
-}
->>>>>>> d835ef45b91d83b76f89cbc20bbd68887a7a60a4
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 const express = require('express');
 const {
@@ -52,62 +51,4 @@ app.get('/blog' , (req, res)=>{
     res.redirect('https://medium.com/@sdanielkenan')
 })
 
-=======
-
-const express = require('express');
-const {
-    exec
-} = require('child_process');
-const app = express();
-const port = process.env.PORT || 3000 ;
-const ejs = require('ejs');
-
-app.set('view engine','ejs');
-
-app.use(express.static('public'));
-app.use(express.urlencoded({
-    extended: false
-}))
-app.get('/', (req, res) => {
-    res.render('index')
-})
-app.get('/home', (req, res) => {
-    res.render('index')
-})
-app.get('/resume',(req,res)=>{
-    res.render('resume')
-})
-
-app.get('/preview/:project_name',(req,res)=>{
-    const {project_name} = req.params;
-    const name = project_name
-    
-    if (name == 'curate')   res.render('curate') ;
-    if (name == 'racing')   res.render('racing') ;
-    if (name == 'construction')   res.render('construction') ;
-    if (name == 'eco')   res.render('eco') ;
-    if (name == 'portfolio')   res.render('portfolio') ;
-    if (name == null) res.redirect('/'); 
-    else {res.redirect('/')}
-})
-
-
-
-
-app.get('/blog/:page', (req, res)=>{
-    let menu = req.query;
-    menu = Object.keys(menu).length == true ? true : false ;
-   
-    if (req.params.page == "story")  res.render('story',{menu:menu}); 
-    if (req.params.page == "git")  res.render('git',{menu:menu});
-    if (req.params.page == "network")  res.render('Networking4SoftwareEngineers',{menu:menu});
-    if (req.params.page == "jspro")  res.render('debugging',{menu:menu});
-    
-})
-    
-app.get('/blog' , (req, res)=>{
-    res.render('blog');
-})
-
->>>>>>> d835ef45b91d83b76f89cbc20bbd68887a7a60a4
 app.listen(port);
